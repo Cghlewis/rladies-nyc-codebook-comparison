0ef0965e89cc2e2064db3a8d8ff8591736c3169c
Cghlewis <33465957+Cghlewis@users.noreply.github.com>
2024-08-10 13:39:04 -0500

    Update 00_codebookr.R

--- code/00_codebookr.R
+++ code/00_codebookr.R
@@ -5,4 +5,4 @@ source(here::here("data.R"))
 
 study_codebook <- codebook(survey, title = "Pet Relationship Study", subtitle = "Various Authors", description = "This study was funded by the Pet Society. Here is a basic description of our study, our methods, our sample, and protocols.")
 
-print(study_codebook, here::here("code","00_my_codebookr_codebook.docx"))
+print(study_codebook, here::here("code","my_codebookr_codebook.docx"))
