4e168dba65a6ad995cfc2215562d37f35728f9c2
Cghlewis <33465957+Cghlewis@users.noreply.github.com>
2024-08-13 06:08:11 -0500

    Update and rename 01_memisc.R to 03_memisc.R

--- code/03_memisc.R
+++ code/03_memisc.R
@@ -5,4 +5,4 @@ source(here::here("data.R"))
 
 study_codebook <- memisc::codebook(survey)
 
-memisc::Write(study_codebook, file = here::here("code","01_my_memisc_codebook.txt"))
+memisc::Write(study_codebook, file = here::here("code","03_my_memisc_codebook.txt"))
